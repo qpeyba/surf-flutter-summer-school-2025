7708f53d5998cd0ad530687d05bf1e4f3e427e8d
Artem Sumin <qpeyba@ya.ru>
2025-08-02 17:48:22 +0300

    oops

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutterVersionCode.toInt()
-        versionName = flutterVersionName
+        versionCode = 1
+        versionName = "App-Dev1"
     }
 
     buildTypes {
