117d6c808dd054a9daf0ceae67c008b801007eb9
Artem Sumin <qpeyba@ya.ru>
2025-08-03 17:21:57 +0300

    feat: update SplashWM to include BuildContext and handle splash state changes

--- lib/features/splash/screens/splash_wm.dart
+++ lib/features/splash/screens/splash_wm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:surf_places/features/splash/domain/entities/splash_state.dart';
 import 'package:surf_places/features/splash/ui/screens/splash_model.dart';
 
@@ -10,9 +11,11 @@ abstract class ISplashWM {
 
 final class SplashWM implements ISplashWM {
   final ISplashModel _model;
+  final BuildContext _context;
 
-  SplashWM({required ISplashModel model}) : _model = model {
-    _model.initializeSplash().then((_) => _handleSplashCompleted());
+  SplashWM({required ISplashModel model, required BuildContext context}) : _model = model, _context = context {
+    _model.splashStateListenable.addListener(_handleStateChange);
+    _model.initializeSplash();
   }
 
   @override
@@ -20,13 +23,18 @@ final class SplashWM implements ISplashWM {
 
   @override
   void dispose() {
+    _model.splashStateListenable.removeListener(_handleStateChange);
     _model.dispose();
   }
 
-  void _handleSplashCompleted() {
+  void _handleStateChange() {
     final state = _model.splashStateListenable.value;
     if (state is SplashStateCompleted) {
-      // TODO: afsdfasddf
+      if (state.isFirstLaunch) {
+        Navigator.pushReplacementNamed(_context, '/onboarding');
+      } else {
+        Navigator.pushReplacementNamed(_context, '/places');
+      }
     }
   }
 }
